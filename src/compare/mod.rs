b4123b7e72419daf951e8d282a42dd4e4d050417
Paul Ruane <paul.ruane@oniony.com>
2024-06-03 23:42:12 +0100

    fix(routines): Fix build

--- src/compare/mod.rs
+++ src/compare/mod.rs
@@ -399,7 +399,7 @@ impl Comparer {
         }
     }
 
-    fn compare_option_property_ignore_whitespace(&mut self, mut path: Vec<Thing>, property_name: &str, left_value: &Option<T>, right_value: &Option<T>) -> ReportEntry
+    fn compare_option_property_ignore_whitespace<T>(&mut self, mut path: Vec<Thing>, property_name: &str, left_value: &Option<T>, right_value: &Option<T>) -> ReportEntry
         where T: PartialEq, T: Display {
         path.push(Property(String::from(property_name)));
         
